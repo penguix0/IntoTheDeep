c8e6e932efda5e1eb01434603d1b8ccf94a983eb
penguix0 <90452358+penguix0@users.noreply.github.com>
2022-09-08 16:24:38 +0200

    improved the jump very much

--- Assets/Player/Script/movement.gd
+++ Assets/Player/Script/movement.gd
@@ -4,32 +4,47 @@ extends KinematicBody2D
 export var controller = 0
 
 const UP = Vector2(0, 1)
-export var slope_stop = 64
+export(int) var slope_stop = 64
 
 var velocity = Vector2()
-export var move_speed = 5 * 96
-export var acceleration = 0.2
-export var gravity = 1200
-export var jump_velocity = -720
+export(int) var moveSpeed = 200
+export(float) var acceleration = 0.5
+export(float) var decceleration = 0.7
+export(float) var decceleration_air = 0.1
+export(float) var walkingThreshold = 0.2
+
+export(float) var gravity = 0
+export(float) var TimeToJumpPeak = 0.3
+export(int) var JumpHeight = 70
+export(float) var JumpSpeed = 0
+
 var is_grounded
+var jumping
+
 onready var raycasts = $raycasts
+onready var AnimPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	AnimPlayer.play("idle")
+	gravity = (2*JumpHeight)/pow(TimeToJumpPeak, 2)
+	JumpSpeed = gravity * TimeToJumpPeak
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_get_input()
-	
-	move_and_slide(velocity, UP, slope_stop)
-	
 	is_grounded = _check_is_grounded()
 	
 	if not is_grounded:
 		_apply_gravity(delta) 
-
+	
+	_get_input()
+	
+	move_and_slide(velocity, UP, slope_stop)
+	
+	if is_grounded and velocity.x > -0.2 and velocity.x < 0.2:
+		AnimPlayer.play("Idle")
+	
 
 func _apply_gravity(delta):
 	velocity.y += gravity * delta
@@ -38,12 +53,13 @@ func _check_is_grounded():
 	for raycast in raycasts.get_children():
 		if raycast.is_colliding():
 			return true
-
 	return false
 	
-func _input(event):
-	if event.is_action_pressed(str(controller)+"_jump") and is_grounded:
-		velocity.y = jump_velocity
+func jump():
+	velocity.y = -JumpSpeed
+	if velocity.x > -walkingThreshold and velocity.x < walkingThreshold:
+		AnimPlayer.play("jump")
+	jumping = true
 	
 func _get_input():
 	var left = int(Input.is_action_pressed(str(controller)+"_left"))
@@ -52,4 +68,26 @@ func _get_input():
 	var move_direction = -(left-right)
 	
 	## The Lerp function smooths out the velocity, this prevents instand acceleration
-	velocity.x = lerp(velocity.x, move_direction * move_speed, acceleration)
+	if move_direction != 0:
+		velocity.x = lerp(velocity.x, move_direction * moveSpeed, acceleration)
+	else:
+		if not is_grounded:
+			velocity.x = lerp(velocity.x, 0, decceleration_air)
+		else:
+			velocity.x = lerp(velocity.x, 0, decceleration)
+	
+	
+	
+	if Input.is_action_pressed(str(controller)+"_jump") and is_grounded:
+		jump()
+	
+	## Stop the jump if jump is released and not already falling down
+	if jumping and Input.is_action_just_released(str(controller)+"_jump") and velocity.y < 0:
+		velocity.y = 0
+		jumping = false
+		
+
+	## Check if the velocity on the y-axis is not bigger than it should be
+	if velocity.y > gravity:
+		velocity.y = gravity
+	
