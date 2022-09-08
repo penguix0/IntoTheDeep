6e9d633c4dc22e0cec6591ead0ba7427eced776a
penguix0 <90452358+penguix0@users.noreply.github.com>
2022-09-08 21:13:09 +0200

    added running

--- Assets/Player/Script/movement.gd
+++ Assets/Player/Script/movement.gd
@@ -8,10 +8,19 @@ export(int) var slope_stop = 64
 
 var velocity = Vector2()
 export(int) var moveSpeed = 200
+var currentMoveSpeed = moveSpeed
+export(float) var runMultiplier = 2
 export(float) var acceleration = 0.5
 export(float) var decceleration = 0.7
 export(float) var decceleration_air = 0.1
 export(float) var walkingThreshold = 0.2
+
+export(float) var stamina = 100
+export(float) var staminaDrainRunning = 25
+export(float) var staminaRegenerationFactor = 10
+export(float) var staminaRegenerationFactorWithTimeOut = 5
+var currentStamina = stamina
+var staminaTimeOut = false
 
 export(float) var gravity = 0
 export(float) var TimeToJumpPeak = 0.3
@@ -21,6 +30,7 @@ export(float) var jumpTimeOffPlatform = 0.1
 
 var is_grounded
 var jumping
+var running
 
 onready var raycasts = $raycasts
 onready var AnimPlayer = $AnimationPlayer
@@ -34,9 +44,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 	## If the player was on the ground and is not anymore
 	if is_grounded and not _check_is_grounded():
+		jumping = false
 		wasGroundedTimer.wait_time = jumpTimeOffPlatform
 		wasGroundedTimer.start()
 		
@@ -45,13 +55,12 @@ func _process(delta):
 	if not is_grounded:
 		_apply_gravity(delta) 
 	
-	_get_input()
+	_get_input(delta)
 	
 	move_and_slide(velocity, UP, slope_stop)
 	
 	if is_grounded and velocity.x > -0.2 and velocity.x < 0.2:
 		AnimPlayer.play("Idle")
-	
 
 func _apply_gravity(delta):
 	velocity.y += gravity * delta
@@ -68,7 +77,7 @@ func jump():
 		AnimPlayer.play("jump")
 	jumping = true
 	
-func _get_input():
+func _get_input(delta):
 	var left = int(Input.is_action_pressed(str(controller)+"_left"))
 	var right = int(Input.is_action_pressed(str(controller)+"_right"))
 
@@ -76,13 +85,52 @@ func _get_input():
 	
 	## The Lerp function smooths out the velocity, this prevents instand acceleration
 	if move_direction != 0:
-		velocity.x = lerp(velocity.x, move_direction * moveSpeed, acceleration)
+		velocity.x = lerp(velocity.x, move_direction * currentMoveSpeed, acceleration)
 	else:
 		if not is_grounded:
 			velocity.x = lerp(velocity.x, 0, decceleration_air)
 		else:
 			velocity.x = lerp(velocity.x, 0, decceleration)
 	
+	### Sprinting
+	
+	if Input.is_action_pressed(str(controller)+"_run") and velocity.x != 0 and currentStamina > 0 and not staminaTimeOut:
+		running = true
+	elif Input.is_action_just_released(str(controller)+"_run") and running or (velocity.x < moveSpeed and velocity.x > moveSpeed):
+		running = false
+	
+	if running:
+		## Multiply the moveSpeed
+		currentMoveSpeed = moveSpeed * runMultiplier
+		## Drain stamina when moving
+		currentStamina -= delta * staminaDrainRunning
+	else:
+		currentMoveSpeed = moveSpeed
+	
+	
+	## Stamina
+	## If stamina is empty activate low stamina mode
+	if currentStamina < 1:
+		staminaTimeOut = true
+		running = false
+	## Regenerate stamina slower when there's almost none left
+	if currentStamina < 0.1*stamina and staminaTimeOut:
+		currentStamina += delta * staminaRegenerationFactorWithTimeOut
+	if currentStamina > 0.1*stamina and staminaTimeOut:
+		staminaTimeOut = false
+			
+	## If stamina is not the maximum regenerate stamina
+	if currentStamina <= stamina and not running and not staminaTimeOut:
+		currentStamina += delta * staminaRegenerationFactor
+	
+	## If the currentStamina value reaches above the original set value: reset it
+	if currentStamina > stamina:
+		currentStamina = stamina
+	
+	$ProgressBar.value = currentStamina
+	
+	
+	### Jumping
 	
 	## Jump when jump is pressed
 	if Input.is_action_pressed(str(controller)+"_jump"):
@@ -93,13 +141,11 @@ func _get_input():
 		elif is_grounded:
 			jump()
 
-	
 	## Stop the jump if jump is released and not already falling down
 	if jumping and Input.is_action_just_released(str(controller)+"_jump") and velocity.y < 0:
 		velocity.y = 0
 		jumping = false
 		
-
 	## Check if the velocity on the y-axis is not bigger than it should be
 	if velocity.y > gravity:
 		velocity.y = gravity
