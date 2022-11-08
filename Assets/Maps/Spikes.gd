514132056d543da1ce1b2b103ff32a59300369c0
penguix0 <90452358+penguix0@users.noreply.github.com>
2022-11-08 19:27:48 +0100

    moved spikes

--- Assets/Maps/Spikes.gd
+++ Assets/Maps/Spikes.gd
@@ -1,21 +1,5 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Spikes_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if "health" in body:
 		body.health = 0
