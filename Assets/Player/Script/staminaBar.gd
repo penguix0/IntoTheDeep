025a1cbe816c5e232454c8a625f3b9a6821838b2
penguix0 <90452358+penguix0@users.noreply.github.com>
2022-11-14 10:43:53 +0100

    disabled stamina

--- Assets/Player/Script/staminaBar.gd
+++ Assets/Player/Script/staminaBar.gd
@@ -16,12 +16,12 @@ onready var animPlayer = $AnimationPlayer
 
 func _on_staminaBar_value_changed(_value):
 	if not active:
-		animPlayer.play("staminaBar_appear")
+		##animPlayer.play("staminaBar_appear")
 		active = true
 	
 func _process(delta):
 	if value == max_value and active:
-		animPlayer.play("staminaBar_disappear")
+		##animPlayer.play("staminaBar_disappear")
 		active = false
 	
 	## If stamina is empty activate low stamina mode
